701055793e5a5ceed8c717917326cb19e43748e9
Decio <decio.crf@gmail.com>
2017-08-18 16:13:15 -0300

    alterado o redirecionamento do registro

--- resources/views/client/registrado.blade.php
+++ resources/views/client/registrado.blade.php
@@ -9,31 +9,7 @@
     <p> Voce será redirecionado para o site do camarotes em 10 segundos.. <p>
     <progress value="0" max="10" id="progressBar"></progress>
     <p> ou clique aqui para ir direto: <a href="http://camarotes.carnatal.com.br">Camarotes</a>
-    {{-- <h3>Bem vindo, complete o cadastro para registrar seu interesse em reservar um camarote.</h3>
-        <form id="fromLP" action="{{ route('web.clients.storeFromLP') }}" method="post" class="form-group">
-            {{ csrf_field() }}
-            <label for="name">Nome:</label>
-            <input class="form-control" type="text" name="name" placeholder="Nome">
-            <label for="phone">Telefone:</label>
-            <input class="form-control" type="text" name="phone" placeholder="Telefone">
-            <label for="email">Email:</label>
-            <input class="form-control" type="text" name="email" placeholder="Email">
-            <label for="social_number">CPF:</label>
-            <input class="form-control" type="text" name="social_number" placeholder="CPF">
-            </br>
-            <div class="pull-right">
-                <input class="btn btn-primary " type="submit">
-                <a class="btn btn-primary" href="http://camarotes.carnatal.com.br">Cancelar</a>
-            </div>
-        </form>   
-
-        @if(session()->has('message'))
-            <div class="alert alert-success">
-                {{ session()->get('message') }}
-            </div>
-        @endif                      
-    --}}
-
+    
 </div>
 
 @endsection
@@ -43,9 +19,15 @@
 var timeleft = 10;
 var downloadTimer = setInterval(function(){
   document.getElementById("progressBar").value = 10 - --timeleft;
-  if(timeleft <= 0)
-    clearInterval(downloadTimer);
-    window.location = "http://camarotes.carnatal.com.br";
+  if(timeleft <= 0){
+    clearInterval(downloadTimer); 
+    if ( document.getElementById("progressBar").value == 10)
+        window.location = "http://camarotes.carnatal.com.br";
+  }
+    
 },1000);
+
+    
+
 </script>
 @endsection
