0dc1bb969098e83db6f2d718bd3680df2048c9ce
kaz <kaz@localhost.localdomain>
2017-08-25 13:39:18 -0300

    Ultimas mudanças

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['auth'], ['web']], function () {
 
     Route::get('/home', 'HomeController@index')->name('home');
 
